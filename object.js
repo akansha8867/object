ae5a5aca60d7523fbd143f6dbd174b2df2bfda80
akansha8867 <akansharana2000@gmail.com>
2020-05-08 20:53:02 +0530

    added new file

---
+++ object.js
@@ -0,0 +1,17 @@
+function object() 
+{
+    let animal={
+        name:"Unicorn"
+    }
+    document.getElementById("obj").innerHTML=animal.name;
+    document.getElementById("obj").style.animation.bold;
+
+    let animal2={
+        about: "A Unicorn is a mythical and legendary animal depicted as a beautiful horse with a single horn.Unicorns are earthbound and do not have wings."
+    }
+    document.getElementById("obj2").innerHTML=animal2.about;
+    
+
+    
+
+}
